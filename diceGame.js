d1bc450f6cf6e604dd7e3c8e3d3e7ffb7b40e68e
Ishouq <41994993+Ishouq@users.noreply.github.com>
2020-10-19 19:11:36 +0300

    Add files via upload

---
+++ diceGame.js
@@ -0,0 +1,26 @@
+function rollDice() {
+
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  document.getElementsByClassName("player1")[0].setAttribute("src", randomNumber1 + ".png");
+  document.getElementsByClassName("player2")[0].setAttribute("src", randomNumber2 + ".png");
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector(".winner").innerHTML = "Player 1 is the winner";
+    document.getElementsByClassName("winPic")[0].setAttribute("src", "winner.png");
+
+    var audio = new Audio('applause.mp3');
+    audio.play();
+
+  } else if(randomNumber1 < randomNumber2) {
+    document.querySelector(".winner").innerHTML = "Player 2 is the winner";
+    document.getElementsByClassName("winPic")[0].setAttribute("src", "winner.png");
+
+    var audio1 = new Audio('applause.mp3');
+    audio1.play();
+
+  } else{
+    document.querySelector(".winner").innerHTML = "TIE";
+  }
+}
